b3b51ca8cfbc147a45370c9a46c126cd44b45e36
MarronFrui <maxime.fels@gmail.com>
2026-08-05 08:33:46 +0200

    Fix: Prefered rem unite instead of vh for styling

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,11 @@
 import './Card.scss';
+import data from '../../data/data.json';
 
 export default function Card() {
   return (
     <>
       <div className="card">
-        <p className="card-title">Placeholder</p>
+        <p className="card-title">{data.id}</p>
       </div>
     </>
   );
