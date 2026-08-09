2835c54188f90e3ceae40c946737ffdd855f454e
MarronFrui <maxime.fels@gmail.com>
2026-08-09 18:46:04 +0200

    Feat: animation logic

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,13 +1,21 @@
 import './Collapse.scss';
+import { useState } from 'react';
 
 export default function Collapse() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
-      <div className="collapse">
+      <div
+        className="collapse"
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
         <div> Placeholder </div>
         <img src="../../src/assets/arrow.png"></img>
       </div>
-      <div className="equipement">
+      <div className={`equipement ${isOpen ? '' : 'hidden'}`}>
         <li>Placeholder</li>
         <li>Placeholder</li>
         <li>Placeholder</li>
