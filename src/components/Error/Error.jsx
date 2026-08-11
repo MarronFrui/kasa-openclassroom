24dcb5f96781cf5d5b648cd4ad0f68e4758c4db1
MarronFrui <maxime.fels@gmail.com>
2026-08-11 17:08:58 +0200

    Feat: Structure for the Error page

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,3 +1,16 @@
+import { Link } from 'react-router-dom';
+import './Error.scss';
+
 export default function Error() {
-  return;
+  return (
+    <>
+      <section>
+        <div className="Error">404</div>
+        <div>La page que vous avez demander n'existe pas</div>
+        <Link to="/" className="link">
+          Retourner sur la page d'accueil
+        </Link>
+      </section>
+    </>
+  );
 }
