d38546e31d3c8ac2ec7e21e3e85159ccdfcc5a8e
MarronFrui <maxime.fels@gmail.com>
2026-08-15 20:59:50 +0200

    Feat: Sticky footer

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,16 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import './Layout.scss';
 
 function Layout() {
   return (
-    <>
+    <div className="container">
       <Header />
       <main>
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
 
