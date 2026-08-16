73b5e7be038165b1a7db52847ca617a8c3620711
MarronFrui <maxime.fels@gmail.com>
2026-08-16 18:18:41 +0200

    Feat: Cards are now clickable to allow navigation

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,14 @@
+import { Link } from 'react-router-dom';
 import './Card.scss';
 
-export default function Card({ title, cover }) {
+export default function Card({ id, title, cover }) {
   return (
     <>
       <div className="card">
-        <img className="card-image" src={cover} alt="" width={100} height={100}></img>
-        <p className="card-title">{title}</p>
+        <Link to={`/item/${id}`} className="card-link">
+          <img className="card-image" src={cover} alt="" width={100} height={100}></img>
+          <p className="card-title">{title}</p>
+        </Link>
       </div>
     </>
   );
