9aabe170e68517a9574320b5a8b3814d1f1c1486
MarronFrui <maxime.fels@gmail.com>
2026-08-17 18:05:51 +0200

    Feat: skeleton for Item.jsx

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,3 +1,14 @@
-export default function Item() {
-  return;
+import Collapse from '../Collapse/Collapse';
+
+export default function Item({ title, cover }) {
+  return (
+    <>
+      <img src={cover}></img>
+      <div>{title}</div>
+      <div>Placeholder location</div>
+      <div>Placeholder rating</div>
+      <Collapse title="Description" list={['qsd']}></Collapse>
+      <Collapse title="Equipements" list={['qsd']}></Collapse>
+    </>
+  );
 }
