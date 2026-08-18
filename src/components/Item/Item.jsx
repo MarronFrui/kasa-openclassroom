99b309062cbf4a81face2426efa1ed2f99f57775
MarronFrui <maxime.fels@gmail.com>
2026-08-18 17:09:10 +0200

    Feat: Use useParam to read data.json

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,18 +1,21 @@
 import Collapse from '../Collapse/Collapse';
 import { useParams } from 'react-router';
 import properties from '../../data/data.json';
+import './Item.scss';
 
 export default function Item() {
   const { id } = useParams();
   const property = properties.find((item) => item.id === id);
   return (
     <>
-      <img src={property.cover}></img>
-      <div>{property.title}</div>
-      <div>Placeholder location</div>
-      <div>Placeholder rating</div>
-      <Collapse title="Description" list={['qsd']}></Collapse>
-      <Collapse title="Equipements" list={['qsd']}></Collapse>
+      <section className="item-container">
+        <img src={property.cover}></img>
+        <div className="title">{property.title}</div>
+        <div>{property.location}</div>
+        <div>Placeholder rating</div>
+        <Collapse title="Description" list={[property.description]} />
+        <Collapse title="Equipements" list={property.equipments} />
+      </section>
     </>
   );
 }
